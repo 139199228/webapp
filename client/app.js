2fbbe34b0eccdbfcd383612134328069c681e50d
nan <139199228@qq.com>
2018-03-15 00:21:44 +0800

    tow 20180315

--- client/app.js
+++ client/app.js
@@ -1,3 +1,4 @@
+import React from 'react'
 import ReactDOM from 'react-dom'
 import App from './App.jsx'
-ReactDOM.render(App,document.body);
+ReactDOM.hydrate(<App></App>,document.getElementById('root'));
---
+++ server/server.js
@@ -0,0 +1,12 @@
+const express = require('express')
+const ReactSSR = require('react-dom/server')
+const fs = require('fs')
+const path = require('path')
+const serverEntry = require('../dist/server-entry').default
+const app = express()
+const template = fs.readFileSync(path.join(__dirname,"../dist/index.html"),'utf8')
+app.use('/pubilc',express.static(path.join(__dirname,'../dist')))
+app.get('*' , (req,res) => {
+    const appString = ReactSSR.renderToString(serverEntry)
+    res.send(template.replace('<app></app>',appString))
+}).listen(8889)
